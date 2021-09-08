498f64d0aa56c070dbc3997bc653b367b4f9a018
Pablo Elena <pablo.elena@smfconsulting.es>
2021-09-08 10:41:48 -0300

    ERP-588: Creation of FileExtensions class

---
+++ src/main/groovy/com/etendoerp/jars/FileExtensions.groovy
@@ -0,0 +1,12 @@
+package com.etendoerp.jars
+
+class FileExtensions {
+
+    final static String JAVA  = "java"
+    final static String CLASS = "class"
+    final static String HBM   = "hbm"
+    final static String XML   = "xml"
+
+    final static String HTML = "html"
+
+}
