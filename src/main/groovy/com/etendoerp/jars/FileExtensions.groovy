14c52bb912b0ce027bd8a195343a1ea0f68c8c01
Isaias Battaglia <isaias.battaglia@gmail.com>
2021-09-10 09:37:55 -0300

    A first version of JAR resources adding src-db, src, and web folders

--- src/main/groovy/com/etendoerp/jars/FileExtensions.groovy
+++ src/main/groovy/com/etendoerp/jars/FileExtensions.groovy
@@ -4,16 +4,17 @@ class FileExtensions {
 
     final static String JAVA    = ".java"
     final static String CLASS   = ".class"
-    final static String HBM     = ".hbm"
+    final static String HBM_XML = ".hbm.xml"
     final static String XML     = ".xml"
     final static String HTML    = ".html"
     final static String JRXML   = ".jrxml"
     final static String SRPT    = ".srpt"
     final static String FTL     = ".ftl"
     final static String XSQL    = ".xsql"
-//    final static String FTL   = ".ftl"
-//    final static String FTL   = ".ftl"
-
-
+    final static String XSLT    = ".xslt"
+    final static String PNG     = ".png"
+    final static String JPGE    = ".jpge"
+    final static String CSS     = ".css"
+    final static String GIF     = ".gif"
 
 }
--- src/main/groovy/com/etendoerp/jars/JarCoreGenerator.groovy
+++ src/main/groovy/com/etendoerp/jars/JarCoreGenerator.groovy
@@ -1,10 +1,8 @@
 package com.etendoerp.jars
 
 import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.file.FileTreeElement
+import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.bundling.Jar
-
 
 class JarCoreGenerator {
     static load(Project project) {
@@ -14,11 +12,42 @@ class JarCoreGenerator {
                 project.logger.info("Starting JAR configuration.")
                 def jarTask = (project.jar as Jar)
                 def generated = Utils.loadGeneratedEntitiesFile(project)
-                jarTask.archiveBaseName.set('etendo-core')
+                jarTask.archiveBaseName.set('etendo-core3')
                 //Excluding src-gen
-                jarTask.exclude(PathUtils.fromPackageToPathClass(generated))
+                jarTask.from('build/classes') {
+                    exclude(PathUtils.fromPackageToPathClass(generated))
+                }
+
+                jarTask.from('build/resources') {
+                    into('META-INF')
+                }
+
             }
         }
+
+        project.tasks.register("copyWebResources", Copy) {
+            from ("${project.projectDir}/web",)
+            into "${project.buildDir}/resources/web"
+        }
+
+        project.tasks.register("copyBeans", Copy) {
+            from "${project.projectDir}/modules_core/org.openbravo.base.weld/config/beans.xml"
+            into "${project.buildDir}/resources"
+        }
+        project.tasks.register("copySrcDB", Copy) {
+            from "${project.projectDir}/src-db"
+                exclude "**/*${FileExtensions.JAVA}"
+            into "${project.buildDir}/resources/src-db"
+        }
+
+        project.tasks.register("copySrc", Copy) {
+            from "${project.projectDir}/src"
+            exclude "**/*${FileExtensions.JAVA}"
+            into "${project.buildDir}/resources/src"
+        }
+
         project.jar.dependsOn("jarConfig")
+        project.jarConfig.dependsOn("copyWebResources", "copyBeans", "copySrcDB", "copySrc")
+
     }
 }
