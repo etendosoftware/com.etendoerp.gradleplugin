c8af2f533da10532c3d74f43ae6ec597ebabde3a
Angelo Chaves <angeloechaves@gmail.com>
2021-09-13 17:29:33 -0300

    ERP-587: Prepared plugin to be deployed in Nexus

--- src/main/groovy/com/etendoerp/EtendoPlugin.groovy
+++ src/main/groovy/com/etendoerp/EtendoPlugin.groovy
@@ -4,6 +4,8 @@ import com.etendoerp.jars.JarLoader;
 import com.etendoerp.legacy.EtendoLegacy
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.WarPlugin
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.publish.plugins.PublishingPlugin
 
@@ -11,8 +13,10 @@ class EtendoPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def extension = project.extensions.create('etendo', EtendoPluginExtension)
+        project.getPluginManager().apply(JavaPlugin.class)
         project.getPluginManager().apply(PublishingPlugin.class)
         project.getPluginManager().apply(MavenPublishPlugin.class)
+        project.getPluginManager().apply(WarPlugin.class)
 
         EtendoLegacy.load(project)
         JarLoader.load(project)
