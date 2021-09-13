ad93545622b3e747a11102292945b16a0027d89f
Pablo Elena <pablo.elena@smfconsulting.es>
2021-09-13 15:45:45 -0300

    ERP-597: Creation of tasks to publish a zip version of a module

--- src/main/groovy/com/etendoerp/legacy/EtendoLegacy.groovy
+++ src/main/groovy/com/etendoerp/legacy/EtendoLegacy.groovy
@@ -1,5 +1,6 @@
-package com.etendoerp.legacy;
+package com.etendoerp.legacy
 
+import com.etendoerp.legacy.modules.ModuleZipLoader;
 import org.gradle.api.Project
 
 class EtendoLegacy {
@@ -7,6 +8,7 @@ class EtendoLegacy {
     static void load(Project project) {
 
         LegacyScriptLoader.load(project)
+        ModuleZipLoader.load(project)
 
     }
 }
