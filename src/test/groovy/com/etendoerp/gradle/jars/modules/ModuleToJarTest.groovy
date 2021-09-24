2262f916f05c94ad12d2a5963453d5f2a9c66188
Pablo Elena <pablo.elena@smfconsulting.es>
2021-09-24 13:28:45 -0300

    ERP-588-TEST-MODULE-JAR: Update module jar test to verify class files

--- src/test/groovy/com/etendoerp/gradle/jars/modules/ModuleToJarTest.groovy
+++ src/test/groovy/com/etendoerp/gradle/jars/modules/ModuleToJarTest.groovy
@@ -9,6 +9,8 @@ import spock.lang.Shared
 import spock.lang.TempDir
 import org.gradle.testkit.runner.TaskOutcome
 
+import java.util.zip.ZipFile
+
 class ModuleToJarTest extends EtendoSpecification {
 
     final static String ENVIRONMENTS_LOCATION = "src/test/resources/jars/environments"
@@ -25,18 +27,55 @@ class ModuleToJarTest extends EtendoSpecification {
         FileUtils.copyDirectory(baseDir, testProjectDir)
     }
 
-    @Issue("ERP-")
-    def "Creation of a JAR"() {
-        given: "a module to create its JAR version"
-            def module = "com.test.nontransactional"
+    @Issue("ERP-588")
+    def "Creation of a JAR from a module"() {
+        given: "A module to create its JAR version"
+        def module = "com.test.nontransactional"
+
         when: "The jar task is runned in the module subproject"
-            def result = runTask(":modules:$module:jar", "-Ppkg=$module") as BuildResult
+        def result = runTask(":modules:$module:jar", "-Ppkg=$module") as BuildResult
 
-        then: "The task will complete successfully"
-            result.task(":modules:$module:jar").outcome == TaskOutcome.SUCCESS
-            def jarFile = new File("${testProjectDir.absolutePath}/modules/$module/build/libs/$module-1.0.0.jar")
+        then: "The task will complete successfully."
+        result.task(":modules:$module:jar").outcome == TaskOutcome.SUCCESS
 
-            assert jarFile.exists()
+        and: "The jar file will be created in the /build/libs of the module folder."
+        def jarFile = new File("${testProjectDir.absolutePath}/modules/$module/build/libs/$module-1.0.0.jar")
+        assert jarFile.exists()
 
+        and: "The jar file will contain all the class files."
+        hasAllClass(module, jarFile)
     }
+
+    String[] getJavaFilesFromModule(String module) {
+        def javaFiles = []
+        def moduleSrcLocation = new File("${testProjectDir.absolutePath}/modules/$module/src")
+        moduleSrcLocation.eachFileRecurse {
+            if (it.name.endsWith(".java")) {
+                javaFiles.add(it.absolutePath.replace("${testProjectDir.absolutePath}/modules/$module/src/",""))
+            }
+        }
+        return javaFiles
+    }
+
+    String[] getClassFilesFromJar(File jarFile) {
+        def classFiles = []
+        new ZipFile(jarFile).entries().each {
+            if (it.name.endsWith(".class")) {
+                classFiles.add(it.name)
+            }
+        }
+        return classFiles
+    }
+
+    void hasAllClass(String moduleName, File jarFile) {
+        def javaFiles = getJavaFilesFromModule(moduleName)
+        def classFiles = getClassFilesFromJar(jarFile)
+
+        javaFiles.each {
+            def file = it.replace(".java",".class")
+            assert classFiles.contains(file)
+        }
+        assert true
+    }
+
 }
