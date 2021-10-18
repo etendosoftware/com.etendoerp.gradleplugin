194ee7ff633d81b886f6e7ec01e360111f47cce8
Angelo Chaves <angeloechaves@gmail.com>
2021-10-18 20:15:25 -0300

    ERP-590: Moved the gradle dependency transfer to Ant's classpath from AntLoader to ResolverDependencyLoader

--- src/main/groovy/com/etendoerp/legacy/ant/AntLoader.groovy
+++ src/main/groovy/com/etendoerp/legacy/ant/AntLoader.groovy
@@ -88,47 +88,6 @@ class AntLoader {
                     }
                 }
 
-                /**
-                 * This method gets all resolved dependencies by gradle and passes all resolved jars to ANT tasks
-                 */
-
-                // Note: previously the antClassLoader was used to add classes to ant's classpath
-                // but when the core is a complete jar (with libs) affecting the class loader can cause collisions
-                // (for example, the IOFileFilter of apache commons appears in the core jar and the gradle libs)
-                // Since the defined ant tasks specify a classpath, the code below should be enough
-                // otherwise doing a antClassLoader.addURL for each dependency will bring back the previous behaviour, but it will cause problems
-                // see https://github.com/gradle/gradle/issues/11914 for more info
-                def antClassLoader = org.apache.tools.ant.Project.class.classLoader
-                URL[] antClasses = antClassLoader.getURLs()
-
-                def newPath = []
-                def dependencies = []
-
-                project.configurations.compile.collect {
-
-                    //antClassLoader.addURL it.toURL()
-
-                    newPath.add project.ant.path(location: it)
-                    dependencies.add project.ant.path(location: it)
-
-                    project.ant.properties['testFromGradle'] = it.getAbsolutePath()
-
-                    project.logger.log(LogLevel.INFO, "Gradle library " + it.getName() + " added to gradle.libs ant filelist")
-                }
-
-                // This method gets all compile dependencies and sets them in ant as a file list with id: "gradle.libs"
-                // Ant task build.local.context uses this to copy them to WebContent
-                project.ant.filelist(id: 'gradle.libs', files: dependencies.join(','))
-
-                project.ant.references.keySet().forEach {
-                    if (it.contains("path")) {
-                        newPath.forEach { pth ->
-                            project.logger.log(LogLevel.INFO, "ant reference " + it + " add to classpath " + pth)
-                            project.ant.references[it].add(pth)
-                        }
-                    }
-                }
-
                 /** Call ant setup to prepare environment */
                 project.task("setup") {
                     dependsOn project.loadAntBuild
--- src/main/groovy/com/etendoerp/legacy/dependencies/ResolverDependencyLoader.groovy
+++ src/main/groovy/com/etendoerp/legacy/dependencies/ResolverDependencyLoader.groovy
@@ -24,15 +24,25 @@ class ResolverDependencyLoader {
 
             List<File> jarFiles = ResolverDependencyUtils.getJarFiles(project)
 
+            // Note: previously the antClassLoader was used to add classes to ant's classpath
+            // but when the core is a complete jar (with libs) affecting the class loader can cause collisions
+            // (for example, the IOFileFilter of apache commons appears in the core jar and the gradle libs)
+            // Since the defined ant tasks specify a classpath, the code below should be enough
+            // otherwise doing a antClassLoader.addURL for each dependency will bring back the previous behaviour, but it will cause problems
+            // see https://github.com/gradle/gradle/issues/11914 for more info
             def antClassLoader = org.apache.tools.ant.Project.class.classLoader
             def newPath = []
+            def dependencies = []
             //
             jarFiles.each {
-                antClassLoader.addURL it.toURL()
-            }
-            jarFiles.each {
                 newPath.add project.ant.path(location: it)
+                dependencies.add project.ant.path(location: it)
             }
+
+            // This gets all dependencies and sets them in ant as a file list with id: "gradle.libs"
+            // Ant task build.local.context uses this to copy them to WebContent
+            project.ant.filelist(id: 'gradle.libs', files: dependencies.join(','))
+
             //
             project.ant.references.keySet().forEach {
                 if(it.contains("path")) {
