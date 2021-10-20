7d5bf6e541ec6f977864679cdd6ea40548a0f0e5
Pablo Elena <pablo.elena@smfconsulting.es>
2021-10-20 08:57:42 -0300

    ERP-589-EXTRACT-CONFIG-TEST: Update core version

--- src/test/groovy/com/etendoerp/gradle/jars/EtendoCoreJarSpecificationTest.groovy
+++ src/test/groovy/com/etendoerp/gradle/jars/EtendoCoreJarSpecificationTest.groovy
@@ -5,7 +5,7 @@ import com.etendoerp.gradle.tests.EtendoSpecification
 abstract class EtendoCoreJarSpecificationTest extends EtendoSpecification {
     public final static String ETENDO_CORE_GROUP   = "com.etendoerp.platform"
     public final static String ETENDO_CORE_NAME    = "etendo-core"
-    public final static String ETENDO_CORE_VERSION = "1.0.1-20210921.201022-1"
+    public final static String ETENDO_CORE_VERSION = "[1.0.0,)"
     public final static String ETENDO_CORE_REPO    = "https://repo.futit.cloud/repository/maven-snapshots/"
 
     public final static String CORE = "${ETENDO_CORE_GROUP}:${ETENDO_CORE_NAME}:${ETENDO_CORE_VERSION}"
