3d911a8120257d2ccf49abfff9a6642ff6ee1221
Pablo Elena <pablo.elena@smfconsulting.es>
2021-11-25 15:37:48 -0300

    EPL-13: Creation of 'expandCustomModule'task to only expand one module, this task also will be used when running the core jar tests

---
+++ src/main/groovy/com/etendoerp/legacy/modules/ExpandModules.groovy
@@ -0,0 +1,68 @@
+package com.etendoerp.legacy.modules
+
+import com.etendoerp.jars.PathUtils
+import com.etendoerp.legacy.utils.NexusUtils
+import com.etendoerp.publication.PublicationUtils
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.file.FileTree
+import org.gradle.api.tasks.Sync
+
+class ExpandModules {
+
+    static void load(Project project) {
+
+        project.tasks.register("expandCustomModuleConfig") {
+            doLast {
+                String moduleName = PublicationUtils.loadModuleName(project)
+
+                NexusUtils.askNexusCredentials(project)
+
+                String moduleLocation = PathUtils.createPath(
+                        project.rootDir.absolutePath,
+                        PublicationUtils.BASE_MODULE_DIR,
+                        moduleName
+                )
+
+                def moduleDepsConfig = project.configurations.findByName("moduleDeps")
+
+                // Search module in the moduleDeps config
+                def moduleNamePath = null;
+                moduleDepsConfig.allDependencies.each {
+                    def depModule = "${it.getGroup()}.${it.getName()}"
+                    if (depModule == moduleName) {
+                        moduleNamePath = File.separator + it.getGroup() + File.separator + it.getName() + File.separator
+                    }
+                }
+
+                if (!moduleNamePath) {
+                    throw new IllegalArgumentException("The custom module '${moduleName}' does not exists. \n" +
+                            "Searched in the 'moduleDeps' configuration, make sure it is defined correctly.")
+                }
+
+                File customModuleJarFile = null
+                moduleDepsConfig.getFiles().each {
+                    if (it.absolutePath.contains(moduleNamePath)) {
+                        customModuleJarFile = it
+                        return
+                    }
+                }
+
+                FileTree unzipCustomModule = project.zipTree(customModuleJarFile)
+
+                Task expandCustomTask = project.tasks.named("expandCustomModule").get() as Sync
+                expandCustomTask.from(unzipCustomModule)
+                expandCustomTask.into(project.file(moduleLocation))
+
+            }
+        }
+
+        project.tasks.register("expandCustomModule", Sync) {
+            dependsOn("expandCustomModuleConfig")
+            doLast {
+                project.logger.info("The custom module was expanded successfully.")
+            }
+        }
+
+    }
+}
