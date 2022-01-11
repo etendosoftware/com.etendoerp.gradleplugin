52c62cd9a2f3f4cbe6446422e4282a6c11131579
Pablo Elena <pablo.elena@smfconsulting.es>
2022-01-11 14:48:41 -0300

    EPL-104: Adding flags to plugin extension related to conflict resolutions

--- src/main/groovy/com/etendoerp/EtendoPluginExtension.groovy
+++ src/main/groovy/com/etendoerp/EtendoPluginExtension.groovy
@@ -1,9 +1,19 @@
 package com.etendoerp
 
+import com.etendoerp.core.CoreMetadata
+
 class EtendoPluginExtension {
     String coreVersion = '[20.1.2,)' // default core version
 
     boolean loadCompilationDependencies = false
     boolean loadTestDependencies = false
+
+    // Flag used to ignore loading the source modules to perform resolution conflicts.
+    boolean ignoreSourceModulesResolution = false
+
+    // Flag used to ignore throwing a error if there is conflict resolutions with the Core dependency.
+    boolean forceResolution = false
+    String coreGroup = CoreMetadata.DEFAULT_ETENDO_CORE_GROUP
+    String coreName = CoreMetadata.DEFAULT_ETENDO_CORE_NAME
 
 }
