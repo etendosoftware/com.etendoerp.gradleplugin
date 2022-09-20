94ab4e065cc95bdaf4e354781d9207b9bb3d73a5
Pablo Elena <pablo.elena@smfconsulting.es>
2022-09-20 15:18:32 -0300

    Feature EPL-330: Update test cases

--- src/test/groovy/com/etendoerp/gradle/jars/extractresources/ExtractResourcesOfCoreJarTest.groovy
+++ src/test/groovy/com/etendoerp/gradle/jars/extractresources/ExtractResourcesOfCoreJarTest.groovy
@@ -1,22 +1,15 @@
 package com.etendoerp.gradle.jars.extractresources
 
-import com.etendoerp.gradle.jars.EtendoCoreJarSpecificationTest
-import org.gradle.testkit.runner.TaskOutcome
-import spock.lang.Narrative
+import com.etendoerp.gradle.jars.resolution.EtendoCoreResolutionSpecificationTest
 import spock.lang.TempDir
 import spock.lang.Title
 
 /**
  * This test should use the latest CORE snapshot
- *  // TODO: This test should resolve from EtendoCoreResolutionSpecificationTest
- // TODO: Use latest snapshot
  */
 
 @Title("Test to verify the correct extraction of the resources of a Etendo core jar.")
-@Narrative(""" TODO: Currently this test will fail because the gradle ant class loader is adding the Etendo core library.
-This causes problems because the 'Etendo core' contains classes that are already defined in the 'Gradle project'
-""")
-class ExtractResourcesOfCoreJarTest extends EtendoCoreJarSpecificationTest {
+class ExtractResourcesOfCoreJarTest extends EtendoCoreResolutionSpecificationTest {
 
     final static List<String> CORE_FILES = [
             "src",
@@ -35,18 +28,20 @@ class ExtractResourcesOfCoreJarTest extends EtendoCoreJarSpecificationTest {
         testProjectDir
     }
 
+    @Override
+    String getCoreVersion() {
+        return ETENDO_LATEST_SNAPSHOT
+    }
+
     def "The resources of the core jar are extracted correctly"() {
         given: "A project adding the Etendo core jar dependency."
-        assert buildFile.text.contains(CORE)
+        addRepositoryToBuildFileFirst(SNAPSHOT_REPOSITORY_URL)
 
-        when: "The users runs the 'dependencies' task"
-        def dependenciesTaskResult = runTask(":dependencies","--refresh-dependencies","-DnexusUser=${args.get("nexusUser")}", "-DnexusPassword=${args.get("nexusPassword")}")
+        Map pluginVariables = ["coreVersion" : "'${getCoreVersion()}'", forceResolution : true]
+        loadCore([coreType : "jar", pluginVariables: pluginVariables])
 
-        then: "The task will finish successfully."
-        dependenciesTaskResult.task(":dependencies").outcome == TaskOutcome.SUCCESS
-
-        and: "The output will contain the core dependency."
-        dependenciesTaskResult.output.contains(CORE)
+        and: "The user resolves the core"
+        resolveCore([coreType : "jar", testProjectDir: testProjectDir])
 
         and: "The resources of the Etendo core will be extracted in the 'build/etendo' directory"
         def location = new File("${getProjectDir().absolutePath}/build/etendo")
