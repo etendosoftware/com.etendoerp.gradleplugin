bb1e10b5ed05299cdf9f92adb54584eee5a45521
Ruben Rafael Danielle <ruben.danielle@smfconsulting.es>
2023-10-11 11:40:03 -0300

    Feature EML-364: Make changes requested by GPT Reviewer

--- src/main/groovy/com/etendoerp/copilot/tools/XMLTranslationTool.groovy
+++ src/main/groovy/com/etendoerp/copilot/tools/XMLTranslationTool.groovy
@@ -1,10 +1,12 @@
 package com.etendoerp.copilot.tools
 
 import com.etendoerp.copilot.Constants
-import net.rubygrapefruit.platform.internal.jni.NativeLogger
 import org.gradle.api.Project
-import org.gradle.api.logging.LogLevel
 import org.gradle.internal.resource.transport.http.HttpRequestException
+
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 class XMLTranslationTool {
     static final String TOOL_FILE_NAME = "XML_translation_tool.py"
@@ -20,24 +22,26 @@ class XMLTranslationTool {
                         "modules/${project.findProperty(Constants.PKG_PROPERTY)}/referencedata/translation/"
                 String translationURL = "http://0.0.0.0:${project.ext.get(Constants.COPILOT_PORT_PROPERTY)}/question"
 
-                String translationRequest = "curl -X POST ${translationURL} " +
-                        "-H 'Content-Type: application/json' " +
-                        "-d '{\"question\":\"${translationQuestion}\"}' " +
-                        "--write-out '%{http_code}'"
+                project.logger.info("* Attempting translation...")
+                HttpClient client = HttpClient.newHttpClient()
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(translationURL))
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString("{\"question\":\"${translationQuestion}\"}"))
+                        .build()
 
-                def stdout = new ByteArrayOutputStream()
-                project.exec {
-                    commandLine 'sh', '-c', translationRequest
-                    standardOutput = stdout
-                }
+                try {
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                    int httpCode = response.statusCode()
+                    String responseBody = response.body()
 
-                def stdOutStr = stdout.toString().trim()
-                def httpCode = stdOutStr.substring(stdOutStr.length() - 3).toInteger()
-                def response = stdOutStr.substring(0, stdOutStr.length() - 3)
-                if (httpCode >= 400) {
-                    throw new HttpRequestException("* Translation attempt failed with status code ${httpCode}: ${response}", null)
-                } else {
-                    project.logger.log(LogLevel.LIFECYCLE, response)
+                    if (httpCode >= 400) {
+                        throw new HttpRequestException("* Translation attempt failed with status code ${httpCode}: ${responseBody}", null)
+                    } else {
+                        project.logger.lifecycle(responseBody)
+                    }
+                } catch (Exception e) {
+                    project.logger.error(e.toString())
                 }
             }
         }
