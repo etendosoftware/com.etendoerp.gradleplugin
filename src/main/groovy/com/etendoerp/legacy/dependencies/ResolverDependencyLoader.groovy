063e351b0bc194f2f8c97b6f2a00a82f6291649e
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-12-07 12:57:13 -0300

    Hotfix EPL-858: Dynamic Jars loaded from build folder

--- src/main/groovy/com/etendoerp/legacy/dependencies/ResolverDependencyLoader.groovy
+++ src/main/groovy/com/etendoerp/legacy/dependencies/ResolverDependencyLoader.groovy
@@ -10,6 +10,8 @@ import com.etendoerp.modules.ModulesConfigurationUtils
 import com.etendoerp.publication.configuration.PublicationConfiguration
 import org.gradle.api.Project
 import org.gradle.api.logging.LogLevel
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 class ResolverDependencyLoader {
 
@@ -73,22 +75,32 @@ class ResolverDependencyLoader {
             // otherwise doing a antClassLoader.addURL for each dependency will bring back the previous behaviour, but it will cause problems
             // see https://github.com/gradle/gradle/issues/11914 for more info
             def antClassLoader = org.apache.tools.ant.Project.class.classLoader
-            def newPath = []
             def dependencies = []
+            def files = []
             //
 
             /**
              * aux ant path used to hold gradle jar files
              */
-            project.ant.path(id:'gradle.custom')
-
+            File destDirectory = new File(sourcePath, "./build/lib/runtime")
+            destDirectory.mkdirs()
             jarFiles.each {
-                newPath.add project.ant.path(location: it)
-                dependencies.add project.ant.path(location: it)
-                project.ant.references['gradle.custom'].add(project.ant.path(location: it))
+                // Copy the jar to the runtime directory
+                File destFile = new File(destDirectory, it.name)
+                Files.copy(it.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+                files.add("./" + it.getName())
+                // Add the jar to the ant path to be used to create WebContent
+                dependencies.add(it.absolutePath)
             }
-
-            project.logger.info("* gradle.custom classpath: ${project.ant.references['gradle.custom']}")
+            def classpathJarName = "classpath.jar";
+            // CREATE JAR HERE FROM gradle.custom and create a Manifest with the classpath
+            project.ant.jar(destfile: "${destDirectory.absolutePath}/${classpathJarName}") {
+                manifest {
+                    attribute(name: "Class-Path", value: files.join(' '))
+                }
+            }
+            project.ant.path(id:'gradle.custom')
+            project.ant.references['gradle.custom'].add(project.ant.path(location: new File(destDirectory.absolutePath, classpathJarName)))
 
             /**
              * Creates an Ant property with the value of the gradle Jar paths.
@@ -118,5 +130,12 @@ class ResolverDependencyLoader {
             AntLoader.loadAntFile(project, coreMetadata)
         }
 
+    }
+
+    static String getSourcePath() {
+        def propsFile = new File("config/Openbravo.properties")
+        def props = new Properties()
+        props.load(propsFile.newReader())
+        return props.getProperty("source.path")
     }
 }
