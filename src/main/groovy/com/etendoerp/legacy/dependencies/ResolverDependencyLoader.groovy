8d0d9504d67a98a944f9c58b00bd94a775336783
sebastianbarrozo <sebastian.barrozo@etendo.software>
2024-03-08 10:11:00 -0300

    Issue #34: Fix sonar requests

--- src/main/groovy/com/etendoerp/legacy/dependencies/ResolverDependencyLoader.groovy
+++ src/main/groovy/com/etendoerp/legacy/dependencies/ResolverDependencyLoader.groovy
@@ -12,7 +12,6 @@ import org.gradle.api.Project
 import org.gradle.api.file.FileTree
 import org.gradle.internal.os.OperatingSystem
 
-
 class ResolverDependencyLoader {
 
     final static String CONSISTENCY_CONTAINER = "CONSISTENCY_CONTAINER"
@@ -75,56 +74,59 @@ class ResolverDependencyLoader {
             // otherwise doing a antClassLoader.addURL for each dependency will bring back the previous behaviour, but it will cause problems
             // see https://github.com/gradle/gradle/issues/11914 for more info
             def antClassLoader = org.apache.tools.ant.Project.class.classLoader
-            def dependencies = []
+            List<String> dependencies = []
             //
 
             /**
              * aux ant path used to hold gradle jar files
              */
-            File destDirectory = new File(project.buildDir, "lib")
+            final String LIB_DIR = 'lib'
+            File destDirectory = new File(project.buildDir, LIB_DIR)
             destDirectory.mkdirs()
-            jarFiles.each {
+            jarFiles.each { File jarFile ->
                 // Copy the jar to the runtime directory
-                dependencies.add(it.absolutePath)
+                dependencies.add(jarFile.absolutePath)
             }
-            def files = dependencies
-            def DIRS = [
-                    new File("${project.rootDir.absolutePath}/lib"),
-                    new File("${project.rootDir.absolutePath}/modules"),
-                    new File("${project.rootDir.absolutePath}/modules_core")
+            List<String> files = dependencies
+            final List<File> DIRS = [
+                new File("${project.rootDir.absolutePath}/lib"),
+                new File("${project.rootDir.absolutePath}/modules"),
+                new File("${project.rootDir.absolutePath}/modules_core"),
             ]
-            DIRS.each {
-                if (it.exists()) {
-                    FileTree libFiles = project.fileTree(it).include("**/*.jar")
+            DIRS.each { File dir ->
+                if (dir.exists()) {
+                    FileTree libFiles = project.fileTree(dir).include('**/*.jar') as FileTree
                     // Search recursively for all jars in the lib directory and add to classpath jar
-                    libFiles.each {
-                        files.add(it)
+                    libFiles.each { File lib ->
+                        files.add(lib.absolutePath)
                     }
                 }
             }
-            def classpathJarName = "classpath.jar"
-            def classpathJarFullPath = "${destDirectory.absolutePath}/${classpathJarName}"
-            def strClasspath = ""
-            files.forEach {
+            final String CLASSPATH_JAR_NAME = 'classpath.jar'
+            final String CLASSPATH_JAR_ABSOLUTE_PATH = "${destDirectory.absolutePath}/${CLASSPATH_JAR_NAME}"
+            final String CLASSPATH_SEPARATOR = ' '
+            String strClasspath = ''
+            files.forEach { String file ->
                 if (OperatingSystem.current().isWindows()) {
                     // Windows paths need to be file:/// and replace \ with /
-                    strClasspath += "file:///" + it.toString().replaceAll("\\\\", "/") + " "
+                    strClasspath += 'file:///' + file.toString().replaceAll('\\\\', '/') + CLASSPATH_SEPARATOR
                 } else {
-                    strClasspath += it.toString() + " "
+                    strClasspath += file.toString() + CLASSPATH_SEPARATOR
                 }
             }
             // CREATE JAR HERE FROM gradle.custom and create a Manifest with the classpath
-            project.ant.jar(destfile: "${classpathJarFullPath}") {
+            project.ant.jar(destfile: CLASSPATH_JAR_ABSOLUTE_PATH) {
                 manifest {
-                    attribute(name: "Class-Path", value: strClasspath)
+                    attribute(name: 'Class-Path', value: strClasspath)
                 }
             }
-            project.ant.property(name: "base.lib", location: new File("${project.rootDir}/build", "lib"))
+            project.ant.property(name: 'base.lib', location: new File("${project.rootDir}/build", LIB_DIR))
             //
-            project.ant.path(id:'gradle.custom')
-            project.ant.references['gradle.custom'].add(project.ant.path(location: classpathJarFullPath))
-            project.ant.properties['gradle.custom.dependencies'] = project.ant.references['gradle.custom'].toString()
-            project.ant.project.setProperty("env.GRADLE_CLASSPATH", project.ant.references['gradle.custom'].toString())
+            final String GRADE_CUSTOM = 'gradle.custom'
+            project.ant.path(id: GRADE_CUSTOM)
+            project.ant.references[GRADE_CUSTOM].add(project.ant.path(location: CLASSPATH_JAR_ABSOLUTE_PATH))
+            project.ant.properties['gradle.custom.dependencies'] = project.ant.references[GRADE_CUSTOM].toString()
+            project.ant.project.setProperty('env.GRADLE_CLASSPATH', project.ant.references[GRADE_CUSTOM].toString())
 
             // This gets all dependencies and sets them in ant as a file list with id: "gradle.libs"
             // Ant task build.local.context uses this to copy them to WebContent
