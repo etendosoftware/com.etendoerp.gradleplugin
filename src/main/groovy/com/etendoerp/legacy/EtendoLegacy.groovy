18bb9d1dc6116d8ef5ef7d7a7ac57becb9668264
sebastianbarrozo <sebastian.barrozo@etendo.software>
2025-04-02 16:03:59 -0300

    Feature ETP-1332: Sonar requests

--- src/main/groovy/com/etendoerp/legacy/EtendoLegacy.groovy
+++ src/main/groovy/com/etendoerp/legacy/EtendoLegacy.groovy
@@ -4,12 +4,19 @@ import com.etendoerp.legacy.dependencies.ResolverDependencyLoader
 import com.etendoerp.legacy.modules.ExpandModulesLoader
 import com.etendoerp.legacy.modules.ModuleZipLoader
 import org.gradle.BuildListener
-import org.gradle.BuildResult;
+import org.gradle.BuildResult
 import org.gradle.api.Project
 import org.gradle.api.initialization.Settings
 import org.gradle.api.invocation.Gradle
 
 class EtendoLegacy {
+
+    static final String COLOR_YELLOW = "\u001B[33m"
+    static final String RESET_COLOR = "\u001B[0m"
+    static final int WIDTH_MARGIN = 20
+    static final int WIDTH_LINE = 80
+    static final String LINE = "=" * WIDTH_LINE
+    static final String MARGIN = "-" * WIDTH_MARGIN
 
     static void load(Project project) {
         ExpandModulesLoader.load(project)
@@ -18,57 +25,77 @@ class EtendoLegacy {
         ResolverDependencyLoader.load(project)
 
         project.gradle.addBuildListener(new BuildListener() {
-            void buildStarted(Gradle gradle) {}
-            void settingsEvaluated(Settings settings) {}
-            void projectsLoaded(Gradle gradle) {}
-            void projectsEvaluated(Gradle gradle) {}
+
+            @SuppressWarnings("UnusedMethodParameter")
+            @Override
+            void beforeSettings(Settings settings) {
+                super.beforeSettings(settings)
+                project.logger.debug("Etendo Legacy Plugin: before settings")
+            }
+
+            @SuppressWarnings("UnusedMethodParameter")
+            @Override
+            void settingsEvaluated(Settings settings) {
+                project.logger.debug("Etendo Legacy Plugin: settings evaluated")
+            }
+
+            @SuppressWarnings("UnusedMethodParameter")
+            @Override
+            void projectsLoaded(Gradle gradle) {
+                project.logger.debug("Etendo Legacy Plugin: projects loaded")
+            }
+
+            @SuppressWarnings("UnusedMethodParameter")
+            @Override
+            void projectsEvaluated(Gradle gradle) {
+                project.logger.debug("Etendo Legacy Plugin: projects evaluated")
+            }
+
+            @Override
             void buildFinished(BuildResult result) {
-                try {
-                    def buildFile = project.getBuildFile()
-                    def content = buildFile.getText()
-                    def lines = content.split('\n')
+                project.logger.debug("Etendo Legacy Plugin: build finished with status: ${result.failure ? 'FAILED' : 'SUCCESS'}")
+                if (result.failure) {
+                    project.logger.warn("Skipping plugin version check because the build failed.")
+                    return
+                }
+                def buildFile = project.getBuildFile()
+                def content = buildFile.getText()
+                def lines = content.split('\n')
 
-                    // Pattern for space-separated style (e.g., id 'plugin_id' version 'latest.release')
-                    def pattern1 = ~"id\\s+(['\"'])[^'\"]*\\1\\s+version\\s+(['\"'])latest.release\\2"
+                // Pattern for space-separated style (e.g., id 'plugin_id' version 'latest.release')
+                def pattern1 = ~"id\\s+(['\"'])[^'\"]*\\1\\s+version\\s+(['\"'])latest.release\\2"
 
-                    // Pattern for method call style (e.g., id('plugin_id') version('latest.release'))
-                    def pattern2 = ~"id\\(['\"']([^'\"]*['\"'])\\)\\s+version\\(['\"']latest.release['\"']\\)"
+                // Pattern for method call style (e.g., id('plugin_id') version('latest.release'))
+                def pattern2 = ~"id\\(['\"']([^'\"]*['\"'])\\)\\s+version\\(['\"']latest.release['\"']\\)"
 
-                    def warning = false
-                    for (line in lines) {
-                        def matcher1 = line =~ pattern1
-                        if (matcher1.find()) {
-                            def pluginId = matcher1[0][0].trim()
-                            println "\u001B[33m" + "*".repeat(80) + "\u001B[0m"
-                            println "\u001B[33m" + " " * 10 + "!!! DANGER ZONE ALERT !!!" + " " * 10 + "\u001B[0m"
-                            println "\u001B[33m" + "=".repeat(80) + "\u001B[0m"
-                            println "\u001B[33m[WARNING] PLUGIN '$pluginId' - "
-                            println "\u001B[33m" + " " * 5 + " THIS IS ABSOLUTELY NOT SUPPORTED!!!\u001B[0m"
-                            println "\u001B[33m" + " " * 5 + ">>> FIX IT NOW: USE A SPECIFIC VERSION <<<" + " " * 5 + "\u001B[0m"
-                            println "\u001B[33m" + " " * 5 + ">>> NEXT RELEASE WILL BLOCK COMPILATION <<<" + " " * 5 + "\u001B[0m"
-                            println "\u001B[33m" + "=".repeat(80) + "\u001B[0m"
-                            println "\u001B[33m" + "*".repeat(80) + "\u001B[0m"
-                            warning = true
-                            break
-                        }
-
-                        def matcher2 = line =~ pattern2
-                        if (matcher2.find()) {
-                            def pluginId = matcher2[0][1].trim()
-                            println "\u001B[33m" + "*".repeat(80) + "\u001B[0m"
-                            println "\u001B[33m" + " " * 10 + "!!! DANGER ZONE ALERT !!!" + " " * 10 + "\u001B[0m"
-                            println "\u001B[33m" + "=".repeat(80) + "\u001B[0m"
-                            println "\u001B[33m[WARNING] PLUGIN '$pluginId' - THIS IS ABSOLUTELY NOT SUPPORTED!!!\u001B[0m"
-                            println "\u001B[33m" + " " * 5 + ">>> FIX IT NOW: USE A SPECIFIC VERSION <<<" + " " * 5 + "\u001B[0m"
-                            println "\u001B[33m" + "=".repeat(80) + "\u001B[0m"
-                            println "\u001B[33m" + "*".repeat(80) + "\u001B[0m"
-                            warning = true
-                        }
+                for (line in lines) {
+                    def matcher1 = line =~ pattern1
+                    if (matcher1.find()) {
+                        def pluginId = matcher1[0][0].trim()
+                        message(pluginId, project)
+                        break
                     }
-                } catch (Exception e) {
-                    println "\u001B[31m" + "Error: ${e.message}" + "\u001B[0m"
+
+                    def matcher2 = line =~ pattern2
+                    if (matcher2.find()) {
+                        def pluginId = matcher2[0][1].trim()
+                        message(pluginId, project)
+                        break
+                    }
                 }
             }
         })
+
+    }
+
+    static void message(String pluginId, Project project) {
+        project.logger.warn COLOR_YELLOW + LINE
+        project.logger.warn " " * 10 + "!!! DANGER ZONE ALERT !!!"
+        project.logger.warn LINE
+        project.logger.warn "[WARNING] PLUGIN '$pluginId' - "
+        project.logger.warn MARGIN + "THIS IS ABSOLUTELY NOT SUPPORTED!!!"
+        project.logger.warn MARGIN + ">>> FIX IT NOW: USE A SPECIFIC VERSION <<<"
+        project.logger.warn MARGIN + ">>> NEXT RELEASE WILL BLOCK COMPILATION <<<"
+        project.logger.warn LINE + RESET_COLOR
     }
 }
