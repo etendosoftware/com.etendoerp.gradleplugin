efb1da39a537002f427626c814b24c557c6ec319
etendobot <noreply@smfconsulting.es>
2025-05-20 22:37:45 +0000

    Update version to 2.0.1 :zap:

--- src/main/groovy/com/etendoerp/EtendoPlugin.groovy
+++ src/main/groovy/com/etendoerp/EtendoPlugin.groovy
@@ -23,7 +23,7 @@ import com.etendoerp.publication.git.CloneDependencies
 
 class EtendoPlugin implements Plugin<Project> {
 
-    final static String PLUGIN_VERSION = "2.0.0"
+    final static String PLUGIN_VERSION = "2.0.1"
 
     @Override
     void apply(Project project) {
